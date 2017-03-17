0ad7c0462ac4cf05516bf0a6fed7e26569878d3c
Naoki Takimoto <takiki.truth@gmail.com>
2017-03-17 17:40:06 +0900

    Update UIColorExtension.swift

--- Swift/UIColorExtension.swift
+++ Swift/UIColorExtension.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 extension UIColor {
-    
-    
+
+    struct AppColor {
+        // #colorLiteral(red: 0.4274509804, green: 0.7568627451, blue: 0.6196078431, alpha: 1)
+        static var darkPrimaryColor: UIColor { return UIColor.hex(hexStr: "#689F38", alpha: 1) }
+    }
+
     /// RGB値をUIColorに変換
     ///
     /// - Parameters:
@@ -19,21 +23,21 @@ extension UIColor {
     ///   - b: B値
     ///   - alpha: Alpha値
     /// - Returns: UIColor
-    class func rgb(r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor{
+    class func rgb(r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor {
         return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
     }
-    
-    
+
+
     /// Hex値をUIColorに変換
     ///
     /// - Parameters:
     ///   - hexStr: Hex値
     ///   - alpha: Alpha値
     /// - Returns: UIColor, 存在しない場合は白を返す
-    class func hex ( hexStr : String, alpha : CGFloat) -> UIColor {
+    class func hex ( hexStr: String, alpha: CGFloat) -> UIColor {
         var hexStr = hexStr
         hexStr = hexStr.replacingOccurrences(of: "#", with: "")
-        
+
         if hexStr.characters.count == 3 {
             var newHexStr = ""
             for c in hexStr.characters {
@@ -41,14 +45,14 @@ extension UIColor {
             }
             hexStr = newHexStr
         }
-        
+
         let scanner = Scanner(string: hexStr as String)
         var color: UInt32 = 0
         if scanner.scanHexInt32(&color) {
             let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
             let g = CGFloat((color & 0x00FF00) >> 8) / 255.0
             let b = CGFloat(color & 0x0000FF) / 255.0
-            return UIColor(red:r,green:g,blue:b,alpha:alpha)
+            return UIColor(red: r, green: g, blue: b, alpha: alpha)
         } else {
             print("invalid hex string")
             return UIColor.white
